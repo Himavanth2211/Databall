291b35a2b1f36df13f09df5bbd69c4ed45c0bf63
Himavanth2211 <61676861+Himavanth2211@users.noreply.github.com>
2024-04-23 14:39:52 -0400

    added new files

--- databall_app/urls.py
+++ databall_app/urls.py
@@ -1,3 +1,4 @@
+from django.shortcuts import redirect
 from django.urls import path
 from .views import schedule_practice_view, game_data_view, player_practice_schedule_view, player_game_schedule_view, university_teams_view
 
@@ -7,4 +8,5 @@ urlpatterns = [
     path('player_game_schedule_view/', player_game_schedule_view, name='player_game_schedule_view'),
     path('player_practice_schedule_view/', player_practice_schedule_view, name='player_practice_schedule_view'),
     path('university_teams_view/', university_teams_view, name='university_teams_view'),
+    path('', lambda request: redirect('/admin/', permanent=True)),
 ]
