eb6deb5a0b6e5217bb1afeccea76db6711d2c745
Himavanth2211 <61676861+Himavanth2211@users.noreply.github.com>
2024-04-23 22:18:00 -0400

    added new files

--- databall_app/forms.py
+++ databall_app/forms.py
@@ -1,12 +1,10 @@
 from django import forms
-from django.forms import widgets
-
+from .models import Team, University, Facilities  # Adjust these imports according to your models
 
 class SchedulePracticeForm(forms.Form):
-    TeamID = forms.CharField(label='Team ID', max_length=255)
-    PracticeDate = forms.DateTimeField(label='Practice Date',
-                                       widget=widgets.DateTimeInput(attrs={'type': 'datetime-local'}))
-    PracticeDuration = forms.CharField(label='Practice Duration', max_length=255)
-    FocusArea = forms.CharField(label='Focus Area', max_length=255)
-    UniversityID = forms.CharField(label='University ID', max_length=255)
-    FacilityID = forms.CharField(label='Facility ID', max_length=255)
+    TeamID = forms.ModelChoiceField(queryset=Team.objects.all(), label="Select Team: ")
+    PracticeDate = forms.DateField(widget=forms.widgets.DateInput(attrs={'type': 'date'}), label="Practice Date: ")
+    PracticeDuration = forms.IntegerField(min_value=1, label="Duration in hours: ")
+    FocusArea = forms.CharField(max_length=100, label="Focus Area:")
+    UniversityID = forms.ModelChoiceField(queryset=University.objects.all(), label="Select University: ")
+    FacilityID = forms.ModelChoiceField(queryset=Facilities.objects.all(), label="Select Facility: ")
